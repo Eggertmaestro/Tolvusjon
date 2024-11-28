77a65b4c81999cd8c6211328193ed2df757d5986
Eggertmaestro <54946803+Eggertmaestro@users.noreply.github.com>
2024-11-28 09:57:43 +0000

    Add files via upload

---
+++ assignment3/assignment3_part2.py
@@ -0,0 +1,44 @@
+import cv2
+import torch
+import time 
+
+prev_frame_time = 0
+new_frame_time = 0
+
+model = torch.hub.load('ultralytics/yolov5','custom' , path='/Users/eggertkristjan/Downloads/best.pt')
+cap = cv2.VideoCapture(1)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Perform inference
+    results = model(frame)
+
+    # Render results
+    results.render()  # This adds bounding boxes to the frame
+
+    # Calculating the fps
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+    new_frame_time = time.time() 
+    fps = 1/(new_frame_time-prev_frame_time) 
+    prev_frame_time = new_frame_time 
+
+    # Converting the fps into integer 
+    fps = int(fps) 
+    fps = str(fps) 
+
+    # Putting the FPS count on the frame 
+    cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+
+    # Display the frame
+    cv2.imshow('YOLOv5 Object Detection', frame)
+
+    # Exit on pressing 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and destroy windows
+cap.release()
+cv2.destroyAllWindows()
